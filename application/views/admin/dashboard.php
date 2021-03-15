afa9b8ba79139657c448058b218a72bf0540447d
zeinismalingga <zein.ismalingga@gmail.com>
2021-03-15 18:38:48 +0800

    gitignore

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -5,26 +5,6 @@
             <li class="breadcrumb-item active">Dashboard</li>
         </ol>
         
-        <div class="row">
-            <div class="col-xl-6">
-                <div class="card mb-4">
-                    <div class="card-header">
-                        <i class="fas fa-chart-area mr-1"></i>
-                        Benih Masuk
-                    </div>
-                    <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
-                </div>
-            </div>
-            <div class="col-xl-6">
-                <div class="card mb-4">
-                    <div class="card-header">
-                        <i class="fas fa-chart-bar mr-1"></i>
-                        Benih Keluar
-                    </div>
-                    <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
-                </div>
-            </div>
-        </div>
         <div class="card mb-4">
             <div class="card-header">
                 <i class="fas fa-table mr-1"></i>
